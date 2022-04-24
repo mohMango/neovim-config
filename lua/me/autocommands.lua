0f49bf71069f362319f35a77a5b2f8572d447c83
Mohamed Ibraheem <moh.mango12@gmail.com>
2022-04-24 19:48:39 +0200

    move to lua[BIG_COMMIT]

---
+++ lua/me/autocommands.lua
@@ -0,0 +1,40 @@
+vim.cmd([[
+  augroup _general_settings
+    autocmd!
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200}) 
+    autocmd BufWinEnter * :set formatoptions-=cro
+    autocmd FileType qf set nobuflisted
+    autocmd BufWinEnter * :set laststatus=3
+  augroup end
+
+  augroup _git
+    autocmd!
+    autocmd FileType gitcommit setlocal wrap
+    autocmd FileType gitcommit setlocal spell
+  augroup end
+
+  augroup _markdown
+    autocmd!
+    autocmd FileType markdown setlocal wrap
+    autocmd FileType markdown setlocal spell
+  augroup end
+
+  augroup _auto_resize
+    autocmd!
+    autocmd VimResized * tabdo wincmd = 
+  augroup end
+
+  augroup _transparent
+    autocmd!
+    autocmd ColorScheme * hi Normal guibg=none ctermbg=none
+    autocmd ColorScheme * hi LineNr guibg=none ctermbg=none
+    autocmd ColorScheme * hi Folded guibg=none ctermbg=none
+    autocmd ColorScheme * hi NonText guibg=none ctermbg=none
+    autocmd ColorScheme * hi SpecialKey guibg=none ctermbg=none
+    autocmd ColorScheme * hi VertSplit guibg=none ctermbg=none
+    autocmd ColorScheme * hi SignColumn guibg=none ctermbg=none
+    autocmd ColorScheme * hi EndOfBuffer guibg=none ctermbg=none
+    autocmd ColorScheme * hi WindowSeprator guibg=NONE ctermbg=NONE
+  augroup end
+]])
---
+++ lua/me/telescope.lua
@@ -0,0 +1,31 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+telescope.load_extension('fzy_native')
+telescope.load_extension('project')
+
+telescope.setup {
+  defaults = {
+
+    path_display = { "smart" },
+
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  },
+}
